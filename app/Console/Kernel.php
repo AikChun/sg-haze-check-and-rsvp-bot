8e4cb5e3e67d0584c25128e51e18b4f5d0b7d847
aikchun <aikchun616@gmail.com>
2016-08-01 22:45:14 +0800

    added routes to get updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use Irazasyed\Telegram\Telegram;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
---
+++ app/Http/Controllers/TelegramController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Irazasyed\Telegram\Telegram;
+
+class TelegramController extends Controller
+{
+    protected $telegram;
+
+    public function __construct(Telegram $telegram)
+    {
+        $this->telegram = $telegram;
+    }
+
+
+    public function getUpdates()
+    {
+        $updates = $this->telegram->getUpdates();
+        dd($updates);
+    }
+
+
+}
