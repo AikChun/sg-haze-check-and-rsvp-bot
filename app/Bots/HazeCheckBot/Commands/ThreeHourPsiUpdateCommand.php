c2cefd9bc531a95d4b50e1ce0195d5f8c4fe06c7
aikchun <aikchun616@gmail.com>
2016-08-06 00:06:46 +0800

    Removed all the previous methods and created a 3 hour psi update command using proper sdk command handler

---
+++ app/Bots/HazeCheckBot/Commands/ThreeHourPsiUpdateCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Bots\HazeCheckBot\Commands;
+
+use Telegram\Bot\Actions;
+use Telegram\Bot\Commands\Command;
+
+class ThreeHourPsiUpdateCommand extends Command
+{
+    /**
+     * @var string Command Name
+     */
+    protected $name = "3hrpsi";
+
+    /**
+     * @var string Command Description
+     */
+    protected $description = "Get 3 Hour PSI Updates";
+
+    /**
+     * @inheritdoc
+     */
+    public function handle($arguments)
+    {
+        // This will send a message using `sendMessage` method behind the scenes to
+        // the user/chat id who triggered this command.
+        // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
+        // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
+
+        $apiUrl = sprintf("http://api.nea.gov.sg/api/WebAPI/?dataset=psi_update&keyref=%s", env('NEA_API_KEY'));
+
+        $client = new \GuzzleHttp\Client();
+        $response =  $client->request('GET', $apiUrl );
+        $text = $this->generateMessageFromResponse($response);
+
+        $this->replyWithMessage(['text' => $text]);
+
+        // This will update the chat status to typing...
+        $this->replyWithChatAction(['action' => Actions::TYPING]);
+
+    }
+
+    public function generateMessageFromResponse($response)
+    {
+        $xmlData = simplexml_load_string($response->getBody()->getContents());
+        $jsonData = json_encode($xmlData);
+        $data = json_decode($jsonData,TRUE);
+
+        $text = $data['title'] . "\n\n";
+        $text .= "Time of Record: " . date('D j-n-Y H:i', strtotime($data['item']['region'][0]['record']['@attributes']['timestamp'])) . "\n\n";
+        $text .= "Region: " . "\n\n";
+        $text .= "North - " . $data['item']['region'][0]['record']['reading'][1]['@attributes']['value'] . "\n\n";
+        $text .= "Central - " . $data['item']['region'][2]['record']['reading'][1]['@attributes']['value'] . "\n\n";
+        $text .= "East - " . $data['item']['region'][3]['record']['reading'][1]['@attributes']['value'] . "\n\n";
+        $text .= "West - " . $data['item']['region'][4]['record']['reading'][1]['@attributes']['value'] . "\n\n";
+
+        return $text;
+    }
+
+
+
+
+}
--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use Telegram;
 use Telegram\Bot\Api;
 use App\Bots\HazeCheckBot\Commands\StartCommand;
+use App\Bots\HazeCheckBot\Commands\ThreeHourPsiUpdateCommand;
 
 class TelegramController extends Controller
 {
@@ -15,183 +16,29 @@ class TelegramController extends Controller
     {
         $this->telegram = new Api(env('HAZEBOT_TOKEN'));
         $this->telegram->addCommands([
-           Telegram\Bot\Commands\HelpCommand::class,
-           StartCommand::class
+           StartCommand::class,
+           ThreeHourPsiUpdateCommand::class,
+           Telegram\Bot\Commands\HelpCommand::class
         ]);
     }
 
     public function setWebhook()
     {
        $response = Telegram::setWebhook(['url' => 'https://pickira.com/hazebot/'. env('TELEGRAM_BOT_TOKEN'). '/webhook']);
+
        return $response;
     }
 
     public function webhook()
     {
-        //$update   = $this->telegram->getWebhookUpdates();
-        //$chat_id  = $update->getMessage()->getChat()->getId();
-        //$this->telegram->sendMessage(['chat_id' => $chat_id, 'text' => 'hello.. text message']);
-        $this->telegram->commandsHandler(true);
-        //$update  = Telegram::getWebhookUpdates();
-        //$chat_id = $update->getMessage()->getChat()->getId();
-        //Telegram::sendMessage(['chat_id' => $chat_id, 'text' => 'hello.. text message']);
+        $update = $this->telegram->commandsHandler(true);
+        Log::info(print_r($update, true));
         return response()->json(["status" => "success"]);
-        //$updatesInObject = json_decode($updates, true);
-        //$updates         = $this->rebuildBrokenJson($updatesInObject);
-        //$lastUpdate      = MessageUpdate::orderBy('id', 'desc')->first();
-        //$lastUpdateId    = 0;
-
-        //if(is_object($lastUpdate)) {
-        //    $lastUpdateId = $lastUpdate->update_id;
-        //}
-
-        //// getNewUpdatesSinceLastUpdateId
-        //$hazeCheckBot = new HazeCheckBot;
-
-        //$newUpdates = $hazeCheckBot->filterNewUpdatesSinceLastUpdateId($updates, $lastUpdateId);
-
-        //if(count($newUpdates) < 1 ) {
-        //    return count($newUpdates);
-        //}
-
-        //array_walk($newUpdates,  function($update) {
-        //    $this->handleEachUpdate($update);
-        //});
-
-        //$maxUpdateId = $this->getMaxUpdateId($newUpdates);
-        //$messageUpdate = new MessageUpdate;
-        //$messageUpdate->update_id = $maxUpdateId;
-        //$messageUpdate->save();
-
-        //return 'OK';
     }
 
     public function removeWebhook()
     {
-        $response = Telegram::removeWebhook();
+        $response = $this->telegram->removeWebhook();
     }
-
-    public function replyToMessages()
-    {
-        $updates = Telegram::getUpdates();
-        // find last updateId
-
-    }
-
-    protected function filterNewUpdatesSinceLastUpdateId($updates, $lastUpdateId)
-    {
-        return array_filter($updates, function($update) use($lastUpdateId) {
-            return $update['update_id'] > $lastUpdateId;
-        });
-    }
-
-    protected function getMaxUpdateId($updates)
-    {
-        return max(array_map(function($update){
-            return $update['update_id'];
-        }, $updates));
-
-    }
-
-    protected function handleEachUpdate($update)
-    {
-        $message = [
-            'chat_id' => $update['message']['chat']['id'],
-        ];
-        $command = strtolower($update['message']['text']);
-        $dataset = $this->getDatasetToNea($command);
-
-        if($dataset === false) {
-            return false;
-        }
-
-
-        $apiUrl = sprintf("http://api.nea.gov.sg/api/WebAPI/?dataset=%s&keyref=%s", $dataset, env('NEA_API_KEY'));
-
-        $client = new \GuzzleHttp\Client();
-        $response =  $client->request('GET', $apiUrl );
-        $xmlData = simplexml_load_string($response->getBody()->getContents());
-        $jsonData = json_encode($xmlData);
-        $dataArray = json_decode($jsonData,TRUE);
-
-        $message['text'] = $this->prepareDataIntoText($dataArray, $command);
-
-        $response = Telegram::sendMessage($message);
-
-    }
-
-    protected function getDatasetToNea($type)
-    {
-        $dataset = "";
-
-        switch($type) {
-            case "/get3hrpsi@sghazecheckbot":
-            case "/get3hrpsi":
-                $dataset="psi_update";
-                break;
-            case "/getforecast@sghazecheckbot":
-            case "/getforecast":
-                $dataset="24hrs_forecast";
-                break;
-
-            default:
-                return false;
-
-        }
-
-        return $dataset;
-
-    }
-
-    protected function prepareDataIntoText($data, $command)
-    {
-        //Log::info(print_r($data, true));
-        $pieces = explode("@", $command);
-        $text = "";
-        if($pieces[0] == "/getforecast") {
-            //return var_dump($data['main']);
-            $text = $data['title'] . "\n\n";
-            $text .= $data['main']['title'] . "\n\n";
-            //$text .= $data['main']['forecastIssue']['@attributes']['date'] . "\n\n";
-            //$text .= $data['main']['forecastIssue']['@attributes']['time'] . "\n\n";
-            $text .= $data['main']['validTime'] . "\n\n";
-            $text .= "Temperature: " . "\n\n";
-            $text .= 'High: ' . $data['main']['temperature']['@attributes']['high'] . "\n\n";
-            $text .= 'Low: ' . $data['main']['temperature']['@attributes']['low'] . "\n\n";
-            $text .= $data['main']['forecast'] . "\n\n";
-        }
-
-        if($pieces[0] == "/get3hrpsi") {
-            $text = $data['title'] . "\n\n";
-            $text .= "Time of Record: " . date('D j-n-Y H:i', strtotime($data['item']['region'][0]['record']['@attributes']['timestamp'])) . "\n\n";
-            $text .= "Region: " . "\n\n";
-            $text .= "North - " . $data['item']['region'][0]['record']['reading'][1]['@attributes']['value'] . "\n\n";
-            $text .= "Central - " . $data['item']['region'][2]['record']['reading'][1]['@attributes']['value'] . "\n\n";
-            $text .= "East - " . $data['item']['region'][3]['record']['reading'][1]['@attributes']['value'] . "\n\n";
-            $text .= "West - " . $data['item']['region'][4]['record']['reading'][1]['@attributes']['value'] . "\n\n";
-        }
-        return $text;
-    }
-
-    protected function rebuildBrokenJson($updates)
-    {
-        $newArray = [];
-        $temp = [];
-        foreach($updates as $update) {
-            $temp[] = $update;
-        }
-        for($i=0;$i<count($temp);$i++) {
-            $newEntry = [
-                'update_id' => $temp[$i],
-                'message' => $temp[$i+1],
-            ];
-
-            $newArray[] = $newEntry;
-            $i++;
-        }
-        return $newArray;
-    }
-
-
 
 }
