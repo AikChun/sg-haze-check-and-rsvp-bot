1ac39780712cfe3339b870e28b0416e2664d5763
aikchun <aikchun616@gmail.com>
2016-08-06 17:00:46 +0800

    logging for errors

--- app/Bots/Commands/CommandsUtil.php
+++ app/Bots/Commands/CommandsUtil.php
@@ -5,8 +5,13 @@ namespace App\Bots\Commands;
 use Log;
 class CommandsUtil
 {
-    public static function getMessageChatType($message)
+    public static function getMessageChatId($message)
     {
         Log::info(print_r($message->getChat(), true));
+        if($message->getChat()->getType() == 'supergroup') {
+            return $message->getChat()->getId();
+        }
+
+        return $message->getChat()->getId();
     }
 }
