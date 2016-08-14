909c10989aabcbd1bbc9f4065ee7d792b28ce209
aikchun <aikchun616@gmail.com>
2016-08-14 23:17:14 +0800

    Declared handle method as abstract in AbstractQuestion

--- app/Bots/QuestionProcessor/AbstractQuestion.php
+++ app/Bots/QuestionProcessor/AbstractQuestion.php
@@ -6,7 +6,7 @@ abstract class AbstractQuestion {
     protected $question;
     protected $status;
 
-    public function handle($message);
+    abstract public function handle($message);
 
     public function getQuestion() {
         return $this->question;
